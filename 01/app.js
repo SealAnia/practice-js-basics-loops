7f2ee7b2ac28c62f3305e44f812527024269db19
SealAnia <koshkoanna916@gmail.com>
2024-06-04 14:00:18 +0100

    message

--- 01/app.js
+++ 01/app.js
@@ -1,2 +1,11 @@
 const randomNumber = Math.round(Math.random() * 20);
 console.log(randomNumber);
+
+if(randomNumber < 5) {
+    console.log('Wylosowana liczba jest zbyt mała, aby użyć pętli');
+}
+else if(randomNumber >= 5) {
+    for(let l = randomNumber; l <= randomNumber + 5; l++) {
+        console.log(l);
+    }
+}
--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,21 @@
 const x = 5;
 let result = 0;
+
+console.log('TURA Z IF');
+
+for(let l = 1; l <= x; l++) {
+    console.log('result = ' + result + " + " + l);
+    result = result + l;
+}
+console.log(result);
+
+console.log('TURA Z WHILE');
+
+var k = 1;
+let resultDwa = 0;
+while(k <= x) {
+    console.log('result = ' + resultDwa + " + " + k);
+    resultDwa = resultDwa + k;
+    k++;
+}
+console.log(resultDwa);
